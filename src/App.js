0f64a7bebd4f6a26e07e4f917f4f3d2b2ce59349
Devilal panchal <devilalpanchal551@gmail.com>
2024-04-20 18:34:40 +0530

    add files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h2 className='Codername'>devilal wants to  Fetch Abi Using React js</h2>
+     
+    </div>
+  );
+}
+
+export default App;
