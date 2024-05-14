317cec90ca09ff11719ba3cafd4ae6e4273d4cf4
Devilal panchal <devilalpanchal551@gmail.com>
2024-05-14 14:36:34 +0530

    update

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Landing from './landing';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Landing />
+
+
   </React.StrictMode>
 );
 reportWebVitals();
---
+++ src/landing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const Landing = () => {
+
+    return (
+        <div>
+
+
+            {/* <button>LogOut</button> */}
+        </div>
+    )
+}
+
+export default Landing
